12c07813e52bde415b9dd46e909017450b424491
vuong <vuongdo_777@yahoo.com>
2025-03-23 22:39:01 +0000

    Add database - adding new feature

--- backend/src/managers/auth/guards/apikey.strategy.ts
+++ backend/src/managers/auth/guards/apikey.strategy.ts
@@ -37,21 +37,21 @@ export class ApiKeyStrategy extends PassportStrategy(
   }
 
   async validate(apikey: string): Promise<EUser | false> {
-    const apiValidation = await IsApiKey().safeParseAsync(apikey);
+    let apiValidation = await IsApiKey().safeParseAsync(apikey);
     if (!apiValidation.success) {
       this.logger.warn('Invalid apikey format: ' + apikey);
       return false;
     }
 
-    const apikeyResult = await this.apikeyDB.resolve(apikey);
+    let apikeyResult = await this.apikeyDB.resolve(apikey);
     if (HasFailed(apikeyResult)) {
       this.logger.warn('Invalid apikey: ' + apikey);
       return false;
     }
 
-    const user = EUserBackend2EUser(apikeyResult.user);
+    let user = EUserBackend2EUser(apikeyResult.user);
 
-    const userValidation = await EUserSchema.safeParseAsync(user);
+    let userValidation = await EUserSchema.safeParseAsync(user);
     if (!userValidation.success) {
       this.logger.error('Invalid user: ' + JSON.stringify(user));
       return false;
